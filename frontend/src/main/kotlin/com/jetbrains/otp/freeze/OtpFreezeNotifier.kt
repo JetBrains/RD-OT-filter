779ccd60da31b699afc059e03f8698328159b906
Denis Zaichenko <denis.zaichenko@jetbrains.com>
2026-01-06 17:56:13 +0100

    Replace freeze event with ui-freeze span

--- frontend/src/main/kotlin/com/jetbrains/otp/freeze/OtpFreezeNotifier.kt
+++ frontend/src/main/kotlin/com/jetbrains/otp/freeze/OtpFreezeNotifier.kt
@@ -8,24 +8,37 @@ import com.intellij.notification.NotificationType
 import com.intellij.notification.NotificationsManager
 import com.intellij.openapi.diagnostic.Logger
 import com.jetbrains.otp.span.DefaultRootSpanService
+import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.api.trace.Span
 import java.nio.file.Path
+import java.time.Instant
+import java.util.concurrent.TimeUnit
 
 @Suppress("UnstableApiUsage")
 class OtpFreezeNotifier : FreezeNotifier {
+    private val tracer = GlobalOpenTelemetry.get().getTracer("com.jetbrains.otp.diagnostic")
+
     override fun notifyFreeze(
         event: LogMessage,
         currentDumps: Collection<ThreadDump>,
         reportDir: Path,
         durationMs: Long
     ) {
-        DefaultRootSpanService.currentSpan().addEvent(
-            "ui.frozen",
-            Attributes.of(
-                AttributeKey.stringKey("message"), event.throwable.message
-            )
-        )
+        val endTime = Instant.now()
+        val startTime = endTime.minusMillis(durationMs)
+
+        val span = tracer.spanBuilder("ui-freeze")
+            .setParent(io.opentelemetry.context.Context.current().with(DefaultRootSpanService.currentSpan()))
+            .setStartTimestamp(startTime)
+            .setAllAttributes(
+                Attributes.of(
+                    AttributeKey.stringKey("message"), event.throwable.message,
+                    AttributeKey.longKey("duration.ms"), durationMs
+                )
+            ).startSpan()
+
+        span.end(endTime)
     }
 }
